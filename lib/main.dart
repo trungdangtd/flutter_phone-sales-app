20662db61bf60f9b292a27ddbf9336672da87677
trungdangtd <trungdang1301@gmail.com>
2024-06-09 17:17:14 +0700

    update theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_store_mobile/utils/theme/theme.dart';
-import 'package:get/get_connect/http/src/utils/utils.dart';
 
 void main() {
   runApp(const MainApp());
@@ -13,9 +12,7 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       themeMode: ThemeMode.system,
-      theme: ThemeData(
-        textTheme: TAppTheme.lightTheme.textTheme,
-      ),
+      theme: TAppTheme.lightTheme,
       darkTheme: ThemeData(),
     );
   }
