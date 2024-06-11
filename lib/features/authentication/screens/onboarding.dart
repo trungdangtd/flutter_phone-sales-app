a29ca9b5ed10fb840c2904a2ff4321c289202b12
trungdangtd <trungdang1301@gmail.com>
2024-06-11 14:37:00 +0700

    update màu

--- lib/features/authentication/screens/onboarding.dart
+++ lib/features/authentication/screens/onboarding.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_store_mobile/utils/constants/colors.dart';
 import 'package:flutter_store_mobile/utils/constants/images_string.dart';
 import 'package:flutter_store_mobile/utils/constants/sizes.dart';
 import 'package:flutter_store_mobile/utils/constants/text_string.dart';
+import 'package:flutter_store_mobile/utils/device/device_utility';
 import 'package:flutter_store_mobile/utils/helpers/helper_function.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 class OnboardingScreen extends StatelessWidget {
   const OnboardingScreen({super.key});
@@ -10,33 +13,98 @@ class OnboardingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          PageView(
-            children: [
-              Column(
-                children: [
-                  Image(
-                      width: THelperFunctions.screenWidth() * 0.8,
-                      height: THelperFunctions.screenHeight() * 0.6,
-                      image: const AssetImage(TImages.onBoardingImage1)),
-                  Text(
-                    TTexts.onBoardingTitle1,
-                    style: Theme.of(context).textTheme.headlineMedium,
-                    textAlign: TextAlign.center,
-                  ),
-                  const SizedBox(
-                    height: TSizes.spaceBtwItems,
-                  ),
-                  Text(
-                    TTexts.onBoardingSubTitle1,
-                    style: Theme.of(context).textTheme.bodyMedium,
-                    textAlign: TextAlign.center,
-                  )
-                ],
-              ),
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topRight,
+            end: Alignment.bottomLeft,
+            colors: [
+              Color(0xFFff4747),
+              // Color(0xFFBFF4BE),
+              Color(0xFFff4747),
             ],
-          )
+          ),
+        ),
+        child: Stack(
+          children: [
+            PageView(
+              children: const [
+                onBoardingPage(
+                    image: TImages.onBoardingImage1,
+                    title: TTexts.onBoardingTitle1,
+                    subTitle: TTexts.onBoardingSubTitle1),
+                onBoardingPage(
+                    image: TImages.onBoardingImage2,
+                    title: TTexts.onBoardingTitle2,
+                    subTitle: TTexts.onBoardingSubTitle2),
+                onBoardingPage(
+                    image: TImages.onBoardingImage3,
+                    title: TTexts.onBoardingTitle3,
+                    subTitle: TTexts.onBoardingSubTitle3),
+                onBoardingPage(
+                    image: TImages.onBoardingImage4,
+                    title: TTexts.onBoardingTitle4,
+                    subTitle: TTexts.onBoardingSubTitle4),
+              ],
+            ),
+            // OnBoardingSkip(),
+            Positioned(
+              bottom: TDeviceUtils.getBottomNavigationBarHeight() + 25,
+              left: TSizes.defaultSpace,
+              child: SmoothPageIndicator(
+                  controller: PageController(),
+                  count: 3,
+                  effect: const ExpandingDotsEffect(
+                      activeDotColor: TColors.dark, dotHeight: 6)),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  // OnBoardingSkip() {}
+}
+
+// ignore: camel_case_types
+class onBoardingPage extends StatelessWidget {
+  const onBoardingPage({
+    super.key,
+    required this.image,
+    required this.title,
+    required this.subTitle,
+  });
+
+  final String image, title, subTitle;
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(TSizes.defaultSpace),
+      child: Column(
+        children: [
+          Image(
+              width: THelperFunctions.screenWidth() * 0.8,
+              height: THelperFunctions.screenHeight() * 0.6,
+              image: AssetImage(image)),
+          Text(
+            title,
+            style: Theme.of(context)
+                .textTheme
+                .headlineLarge
+                ?.copyWith(color: Colors.white),
+            textAlign: TextAlign.center,
+          ),
+          const SizedBox(
+            height: TSizes.spaceBtwItems,
+          ),
+          Text(
+            subTitle,
+            style: Theme.of(context)
+                .textTheme
+                .bodyLarge
+                ?.copyWith(color: Colors.white),
+            textAlign: TextAlign.center,
+          ),
         ],
       ),
     );
--- lib/utils/constants/text_string.dart
+++ lib/utils/constants/text_string.dart
@@ -1,25 +1,26 @@
-
 class TTexts {
-
   // -- GLOBAL Texts
   static const String and = "và";
   static const String skip = "Bỏ qua";
   static const String done = "Hoàn tất";
   static const String submit = "Gửi";
-  static const String appName = "T-Store";
+  static const String appName = "5TechStore";
   static const String tContinue = "Tiếp tục";
 
-
   // -- OnBoarding Texts
-  static const String onBoardingTitle1 = "Chọn sản phẩm của bạn";
+  static const String onBoardingTitle1 = "Chọn sản phẩm \n cho riêng bạn!";
   static const String onBoardingTitle2 = "Chọn phương thức thanh toán";
-  static const String onBoardingTitle3 = "Giao hàng tận nơi";
+  static const String onBoardingTitle3 = "Khám phá sản phẩm đỉnh cao";
+  static const String onBoardingTitle4 = "Giao hàng tận nơi";
 
-  static const String onBoardingSubTitle1 = "Chào mừng đến với Thế Giới 5TechZone - Những Sản Phẩm Hoàn Hảo Đang Chờ Đợi Bạn!";
-  static const String onBoardingSubTitle2 = "Để Giao Dịch Không Giới Hạn, Hãy chọn cách mua của bạn - Sự Tiện Lợi của Bạn Là Ưu Tiên của Chúng Tôi!";
-  static const String onBoardingSubTitle3 = "Bạn sẽ khám phá những bộ sưu tập điện thoại mới nhất từ ​​các thương hiệu hàng đầu, cùng với các thiết bị âm thanh đỉnh cao";
-  static const String onBoardingSubTitle4 = "Từ Cửa Hàng đến Nhà Bạn - Giao Hàng Nhanh Chóng, An Toàn và Không Tiếp Xúc!";
-
+  static const String onBoardingSubTitle1 =
+      "Chào mừng đến với Thế Giới 5TechZone - Những Sản Phẩm Hoàn Hảo Đang Chờ Đợi Bạn!";
+  static const String onBoardingSubTitle2 =
+      "Để Giao Dịch Không Giới Hạn, Hãy chọn cách mua của bạn - Sự Tiện Lợi của Bạn Là Ưu Tiên của Chúng Tôi!";
+  static const String onBoardingSubTitle3 =
+      "Bạn sẽ khám phá những bộ sưu tập điện thoại mới nhất từ ​​các thương hiệu hàng đầu, cùng với các thiết bị âm thanh đỉnh cao";
+  static const String onBoardingSubTitle4 =
+      "Từ Cửa Hàng đến Nhà Bạn - Giao Hàng Nhanh Chóng, An Toàn và Không Tiếp Xúc!";
 
   // -- Authentication Forms
   static const String firstName = "Tên";
@@ -42,21 +43,27 @@ class TTexts {
   static const String resendEmail = "Gửi lại email";
   static const String resendEmailIn = "Gửi lại email trong";
 
-
   // -- Authentication Headings
   static const String loginTitle = "Chào mừng trở lại,";
-  static const String loginSubTitle = "Khám Phá Lựa Chọn Vô Hạn và Tiện Lợi Không Đối Thủ.";
+  static const String loginSubTitle =
+      "Khám Phá Lựa Chọn Vô Hạn và Tiện Lợi Không Đối Thủ.";
   static const String signupTitle = "Hãy tạo tài khoản của bạn";
   static const String forgetPasswordTitle = "Quên mật khẩu";
-  static const String forgetPasswordSubTitle = "Đừng lo lắng, đôi khi mọi người cũng có thể quên, hãy nhập email của bạn và chúng tôi sẽ gửi cho bạn một liên kết đặt lại mật khẩu.";
-  static const String changeYourPasswordTitle = "Email Đặt Lại Mật Khẩu Đã Được Gửi";
-  static const String changeYourPasswordSubTitle = "Bảo mật Tài Khoản của Bạn là Ưu Tiên của Chúng Tôi! Chúng Tôi Đã Gửi Bạn Một Liên Kết An Toàn Để Thay Đổi Mật Khẩu và Giữ An Toàn cho Tài Khoản của Bạn.";
+  static const String forgetPasswordSubTitle =
+      "Đừng lo lắng, đôi khi mọi người cũng có thể quên, hãy nhập email của bạn và chúng tôi sẽ gửi cho bạn một liên kết đặt lại mật khẩu.";
+  static const String changeYourPasswordTitle =
+      "Email Đặt Lại Mật Khẩu Đã Được Gửi";
+  static const String changeYourPasswordSubTitle =
+      "Bảo mật Tài Khoản của Bạn là Ưu Tiên của Chúng Tôi! Chúng Tôi Đã Gửi Bạn Một Liên Kết An Toàn Để Thay Đổi Mật Khẩu và Giữ An Toàn cho Tài Khoản của Bạn.";
   static const String confirmEmail = "Xác nhận địa chỉ email của bạn!";
-  static const String confirmEmailSubTitle = "Chúc Mừng! Tài Khoản của Bạn Đang Chờ Được Xác Nhận: Xác Nhận Email của Bạn Để Bắt Đầu Mua Sắm và Trải Nghiệm Những Ưu Đãi Không Thể Bỏ Qua và Các Ưu Đãi Cá Nhân Hóa.";
-  static const String emailNotReceivedMessage = "Không nhận được email? Kiểm tra hộp thư rác/thư quảng cáo hoặc gửi lại.";
-  static const String yourAccountCreatedTitle = "Tài khoản của bạn đã được tạo thành công!";
-  static const String yourAccountCreatedSubTitle = "Chào Mừng Đến Với Điểm Đến Mua Sắm Tối Ưu: Tài Khoản của Bạn Đã Được Tạo, Khám Phá Niềm Vui Mua Sắm Trực Tuyến Không Gián Đoạn!";
-
+  static const String confirmEmailSubTitle =
+      "Chúc Mừng! Tài Khoản của Bạn Đang Chờ Được Xác Nhận: Xác Nhận Email của Bạn Để Bắt Đầu Mua Sắm và Trải Nghiệm Những Ưu Đãi Không Thể Bỏ Qua và Các Ưu Đãi Cá Nhân Hóa.";
+  static const String emailNotReceivedMessage =
+      "Không nhận được email? Kiểm tra hộp thư rác/thư quảng cáo hoặc gửi lại.";
+  static const String yourAccountCreatedTitle =
+      "Tài khoản của bạn đã được tạo thành công!";
+  static const String yourAccountCreatedSubTitle =
+      "Chào Mừng Đến Với Điểm Đến Mua Sắm Tối Ưu: Tài Khoản của Bạn Đã Được Tạo, Khám Phá Niềm Vui Mua Sắm Trực Tuyến Không Gián Đoạn!";
 
   // -- Product
   static const String popularProducts = "Sản phẩm phổ biến";
