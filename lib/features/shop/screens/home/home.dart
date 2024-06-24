425690cee873771f000de9473d5863c7efd30677
nhoxlovew <hoanganhonzed@gmail.com>
2024-06-24 11:43:24 +0700

    fix homescreen

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_store_mobile/common/widgets/appbar/appbar.dart';
+import 'package:flutter_store_mobile/utils/constants/colors.dart';
+import 'package:flutter_store_mobile/utils/constants/text_string.dart';
 import '../../../../common/widgets/custom_shape/containers/primary_header_container.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -10,7 +13,28 @@ class HomeScreen extends StatelessWidget {
       body: SingleChildScrollView(
         child: Column(
           children: [
-            TPrimaryHeaderContainer(child: Container()),
+            //header
+            TPrimaryHeaderContainer(
+                child: Column(
+              children: [
+                TAppBar(
+                  title: Column(
+                    children: [
+                      Text(TTexts.homeAppbarTitle,
+                          style: Theme.of(context)
+                              .textTheme
+                              .labelMedium!
+                              .apply(color: TColors.grey)),
+                      Text(TTexts.homeAppbarSubTitle,
+                          style: Theme.of(context)
+                              .textTheme
+                              .labelMedium!
+                              .apply(color: TColors.white)),
+                    ],
+                  ),
+                )
+              ],
+            )),
           ],
         ),
       ),
