ce3ea54153e185f3b2705cdd5dbe3aa375a0fefe
trungdangtd <trungdang1301@gmail.com>
2024-06-29 16:33:40 +0700

    container for searchbar

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_store_mobile/common/widgets/custom_shape/containers/search_container.dart';
 import 'package:flutter_store_mobile/features/shop/screens/home/widgets/home_appbar.dart';
+import 'package:flutter_store_mobile/utils/constants/sizes.dart';
+import 'package:iconsax/iconsax.dart';
 import '../../../../common/widgets/custom_shape/containers/primary_header_container.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -16,8 +19,13 @@ class HomeScreen extends StatelessWidget {
                 children: [
                   //AppBar
                   THomeAppBar(),
+                  SizedBox(height: TSizes.spaceBtwSections),
                   //SreachBar
-
+                  TSearchContainer(
+                    text: 'Tìm kiếm sản phẩm',
+                    icon: Iconsax.search_normal,
+                  ),
+                  SizedBox(height: TSizes.spaceBtwSections),
                   //Categories
                 ],
               ),
