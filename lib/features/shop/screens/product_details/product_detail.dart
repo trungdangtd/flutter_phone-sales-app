38bc9b02a987a7826e697a0dd9426e5f653d6ad5
nhoxlovew <hoanganhonzed@gmail.com>
2024-07-12 18:49:13 +0700

    Fix (bỏ các khai báo không cần thiết))

--- lib/features/shop/screens/product_details/product_detail.dart
+++ lib/features/shop/screens/product_details/product_detail.dart
@@ -1,18 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_store_mobile/common/widgets/appbar/appbar.dart';
-import 'package:flutter_store_mobile/common/widgets/custom_shape/containers/rounded_container.dart';
-import 'package:flutter_store_mobile/common/widgets/custom_shape/curved_edges/curved_edges_widget.dart';
-import 'package:flutter_store_mobile/common/widgets/icons/t_circular_icon.dart';
-import 'package:flutter_store_mobile/common/widgets/images/rounded_image.dart';
 import 'package:flutter_store_mobile/features/shop/screens/product_details/widgets/product_detail_image_slider.dart';
 import 'package:flutter_store_mobile/features/shop/screens/product_details/widgets/product_meta_data.dart';
 import 'package:flutter_store_mobile/features/shop/screens/product_details/widgets/rating_share_widget.dart';
-import 'package:flutter_store_mobile/utils/constants/colors.dart';
-import 'package:flutter_store_mobile/utils/constants/images_string.dart';
 import 'package:flutter_store_mobile/utils/constants/sizes.dart';
 import 'package:flutter_store_mobile/utils/helpers/helper_function.dart';
-import 'package:get/get.dart';
-import 'package:iconsax/iconsax.dart';
 
 class ProductDetail extends StatelessWidget {
   const ProductDetail({super.key});
